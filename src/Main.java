3a5984679ebfc9510300911ecd16516c7e96dfae
Raymond Ha <Haro@mail.uc.edu>
2022-10-02 23:27:30 -0400

    Added Formulas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double metricMeters;
+        double metersMiles;
+        double metersFeet;
+        double metersInches;
+        String trash = "";
+
+        System.out.println("Enter a measurement in meters: ");
+
+        if(in.hasNextDouble())
+        {
+            metricMeters = in.nextDouble();
+            metersMiles = metricMeters * 0.000621;
+            metersFeet = metricMeters * 3.28;
+            metersInches = metricMeters * 39.37;
+            System.out.println("The measurement in miles is " + metersMiles);
+            System.out.println("The measurement in feet is " + metersFeet);
+            System.out.println("The measurement in inches is " + metersInches);
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("Incorrect values. Try again.");
+        }
     }
 }
